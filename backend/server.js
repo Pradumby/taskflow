3d59f492aa2167751643574561118369ec3a85cf
Pradumby <anupy08115@gmail.com>
2026-03-06 20:36:29 +0530

    refactor: rewrite app.js and server.js for learning purpose

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
-
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
@@ -10,7 +9,7 @@ const startServer = async () => {
     await connectDB();
 
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server is running on ${PORT}`);
     });
   } catch (error) {
     console.error("Server start failed", error);
--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "TaskFlow API running" });
+  res.json({ message: "API is Running" });
 });
 
 module.exports = app;
