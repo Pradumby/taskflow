aada798d0d34b8bdba4293ddfc81c1369793aa9d
Pradumby <anupy08115@gmail.com>
2026-03-07 22:17:27 +0530

    feat: implement user authentication with JWT and bcrypt

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "API is Running" });
-});
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
